3c7f974c706059592a20a857033a54161366844c
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-10-06 16:39:23 -0400

    Create responsive Navbar component with navigation links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>Future Mobility Hub</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
